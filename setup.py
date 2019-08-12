fd10e2a09de24707bf240bd79519eba0a464bddc
sculla <sculla@u.washington.edu>
2019-08-11 20:27:00 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+"""Setup File"""
+
+setup(
+    name='project4',
+    version='.1',
+    packages=[''],
+    url='https://github.com/sculla/project4',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='sculla',
+    author_email='scull.aa@gmail.com',
+    description='Project 4'
+)
