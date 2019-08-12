718d1ae2eb07a659e31bfc6ab4ae39835aee21b7
sculla <sculla@u.washington.edu>
2019-08-11 22:33:02 -0700

    Adding Tools - load_set() with test coverage
    Signed-off-by: sculla <sculla@u.washington.edu>

--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import numpy as np
 
 
 def load_set():
-    return pd.read_csv('data/data.csv', header=0)
+    loaded_df = pd.read_csv('data/data.csv', header=0)
+    assert pd.core.frame.DataFrame == type(loaded_df), 'load_set failed to output a df'
+    assert loaded_df.__len__() == 36941, 'data is missing, length should be 36941'
+    return loaded_df
